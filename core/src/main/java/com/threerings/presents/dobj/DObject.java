d7d1225edc25995740b30c737d564a0cec2d15de
Charlie Groves <charlie.groves@gmail.com>
2013-02-01 18:25:32 -0500

    Shut up about setting managers

--- core/src/main/java/com/threerings/presents/dobj/DObject.java
+++ core/src/main/java/com/threerings/presents/dobj/DObject.java
@@ -555,7 +555,6 @@ public class DObject
      */
     public void setManager (DObjectManager omgr)
     {
-        log.warning("setting manager", "obj", this);
         _omgr = omgr;
         if (omgr instanceof RootDObjectManager) {
             for (DObject child : getChildren()) {
